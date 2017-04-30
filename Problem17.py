4efefca7eb6f05367bc7cdc5ad65e92b3bd09129
jayala-29 <jayala@ucsd.edu>
2017-04-29 19:04:04 -0700

    Number letter counts

--- Problem17.py
+++ Problem17.py
@@ -1,4 +1,6 @@
-# python implementation
+# If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+# If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+
 zn = {0: 0, 1: 3, 2: 3, 3: 5, 4: 4, 5: 4, 6: 3, 7: 5, 8: 5, 9: 4}
 tnn = {10: 3, 11: 6, 12: 6, 13: 8, 14: 8, 15: 7, 16: 7, 17: 9, 18: 8, 19: 8}
 tn = {2: 6, 3: 6, 4: 5, 5: 5, 6: 5, 7: 7, 8: 6, 9: 6}
